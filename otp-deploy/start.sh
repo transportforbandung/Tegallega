de3119f1ad7501cb61699cfde1f200c917ee0332
NFarrasP <naufalfarrashariyanto@gmail.com>
2025-05-31 00:28:33 +0700

    Docker and startsh for prebuilt docker image

--- otp-deploy/start.sh
+++ otp-deploy/start.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-set -e
-
-# Create necessary folders if not exist
-mkdir -p /app/routers/default
-
-# Copy graph to the correct place for OTP to find
-cp -r ./routers /app/
-
-# Run the official OTP jar (already provided in Docker image)
-exec java -Xmx2G -jar /usr/app/otp-shaded.jar --load /app/routers/default
+exec java -Xmx2G -jar otp-shaded.jar --load --serve
