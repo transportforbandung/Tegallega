a056c51f36e6ebe753eb3837257950a2e9b9831b
NFarrasP <naufalfarrashariyanto@gmail.com>
2025-05-31 00:13:44 +0700

    Mark start.sh as executable

--- otp-deploy/start.sh
+++ otp-deploy/start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-java -Xmx4G -jar otp.jar --serve
+set -e
+
+# Create necessary folders if not exist
+mkdir -p /app/routers/default
+
+# Copy graph to the correct place for OTP to find
+cp -r ./routers /app/
+
+# Run the official OTP jar (already provided in Docker image)
+exec java -Xmx2G -jar /usr/app/otp-shaded.jar --load /app/routers/default
